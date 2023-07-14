a91b30e47c7caa15c667a050f2e2a8d7dbd11704
wyf <1669313568@qq.com>
2023-07-14 19:44:08 +0800

    1

--- src/main/java/structural/flyweight/Main.java
+++ src/main/java/structural/flyweight/Main.java
@@ -3,15 +3,15 @@ package structural.flyweight;
 public class Main {
     public static void main(String[] args) {
         //下黑子
-        Chess backChess1 = ChessFactory.getChess(Color.BLACK.color);
+        structural.flyweight.Chess backChess1 = structural.flyweight.ChessFactory.getChess(Color.BLACK.color);
         backChess1.draw(2.0, 5.0);
-
+//
         //下白子
-        Chess whiteChess = ChessFactory.getChess(Color.WHITE.color);
+        structural.flyweight.Chess whiteChess = structural.flyweight.ChessFactory.getChess(Color.WHITE.color);
         whiteChess.draw(3.0, 5.0);
 
         //下黑子
-        Chess backChess2 = ChessFactory.getChess(Color.BLACK.color);
+        structural.flyweight.Chess backChess2 = structural.flyweight.ChessFactory.getChess(Color.BLACK.color);
         backChess2.draw(2.0, 6.0);
 
         System.out.println(String.format("backChess1:%d | backChess2:%d | whiteChess:%d",
