6ef3cb4c16c310ac0d04032e1ab8d2728a3b2217
wyf <1669313568@qq.com>
2023-07-27 17:31:47 +0800

    FIx

--- src/main/java/creational/patterns/Cat1.java
+++ src/main/java/creational/patterns/Cat1.java
@@ -2,6 +2,7 @@ package creational.patterns;
 
 public class Cat1 extends Cat{
     public void eat1() {
-        System.out.println("猫吃猫粮1");
+        System.out.println("猫吃猫粮11");
     }
+
 }
--- src/main/java/structural/strategy/PromotionStrategyFactory.java
+++ src/main/java/structural/strategy/PromotionStrategyFactory.java
@@ -10,18 +10,17 @@ public class PromotionStrategyFactory {
         PROMOTION_STRATEGY_MAP.put(PromotionKey.MANJIAN, new ManJianPromotionStrategy());
         PROMOTION_STRATEGY_MAP.put(PromotionKey.LIJIAN, new LiJianPromotionStrategy());
         PROMOTION_STRATEGY_MAP.put(PromotionKey.FANXIAN, new FanXianPromotionStrategy());
-
     }
 
     private interface PromotionKey {
         String LIJIAN = "LIJIAN";
-        
         String FANXIAN = "FANXIAN";
         String MANJIAN = "MANJIAN";
     }
 
     public static PromotionStrategy getPromotionStrategy(String promotionKey) {
         PromotionStrategy promotionStrategy =  PROMOTION_STRATEGY_MAP.get(promotionKey);
+        //
         return promotionStrategy == null ? emptyPromotionStrategy : promotionStrategy;
     }
 }
