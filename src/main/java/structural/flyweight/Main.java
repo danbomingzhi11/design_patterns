2923aceb443f35d5be5762dfbd7f3b0c6eaf8d3d
wyf <1669313568@qq.com>
2023-08-22 09:20:59 +0800

    fix bug

--- src/main/java/structural/flyweight/Main.java
+++ src/main/java/structural/flyweight/Main.java
@@ -17,5 +17,6 @@ public class Main {
         System.out.println(String.format("backChess1:%d | backChess2:%d | whiteChess:%d",
                 backChess1.hashCode(), backChess2.hashCode(), whiteChess.hashCode()));
 
+        
     }
 }
