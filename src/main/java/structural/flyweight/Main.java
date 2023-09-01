d135c418214d5ec18091fddc22e2c72ab2585ac9
wyf <1669313568@qq.com>
2023-09-01 17:36:14 +0800

    fix bug

--- src/main/java/structural/flyweight/Main.java
+++ src/main/java/structural/flyweight/Main.java
@@ -6,16 +6,17 @@ public class Main {
         structural.flyweight.Chess backChess1 = structural.flyweight.ChessFactory.getChess(Color.BLACK.color);
         backChess1.draw(2.0, 5.0);
 
+
+        
         //下白子
         structural.flyweight.Chess whiteChess = structural.flyweight.ChessFactory.getChess(Color.WHITE.color);
         whiteChess.draw(3.0, 5.0);
-
 
         //下黑子
         structural.flyweight.Chess backChess2 = structural.flyweight.ChessFactory.getChess(Color.BLACK.color);
         backChess2.draw(2.0, 6.0);
 
-        
+
         System.out.println(String.format("backChess1:%d | backChess2:%d | whiteChess:%d",
                 backChess1.hashCode(), backChess2.hashCode(), whiteChess.hashCode()));
     }
